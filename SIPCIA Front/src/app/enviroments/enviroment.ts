0ff4dbd5b06166ca191e1f999e59e0954ea7e13e
Víctor Trejo Monroy <victor.trejo@iecm.mx>
2025-10-28 11:55:58 -0600

    produccion

--- SIPCIA Front/src/app/enviroments/enviroment.ts
+++ SIPCIA Front/src/app/enviroments/enviroment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   //apiUrl: 'http://145.0.40.23:4002/api/' //DESAROLLO
-  apiUrl: 'https://app.iecm.mx/sipcia/api/' //PROD 
+    apiUrl: 'https://app.iecm.mx/sipcia/api/' //PROD 
   // apiUrl: 'http://145.0.46.49:4000/api/'
-  //apiUrl: 'http://localhost:4000/api/'
+  // apiUrl: 'http://localhost:4000/api/'
 };
